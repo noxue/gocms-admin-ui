1a0769704a97b0cda853c0dbc34565740bb77960
noxue <yes@noxue.com>
2018-10-15 00:46:25 +0800

    完成分类/文章/章节增删改查

--- src/router/index.js
+++ src/router/index.js
@@ -99,7 +99,7 @@ export const constantRouterMap = [
 ]
 
 export default new Router({
-  // mode: 'history', //后端支持可开
+  mode: 'history',
   scrollBehavior: () => ({ y: 0 }),
   routes: constantRouterMap
 })
